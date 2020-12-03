daf3fa28373b59a59115bd8350d05a7f1da85820
Tim <timcpb@hotmail.com>
2020-12-03 22:00:48 +0000

    returns correct numbers form the test array

--- lib/day1.rb
+++ lib/day1.rb
@@ -1,5 +1,15 @@
 require_relative "../constants/day1_expense_report.rb"
 
 def sum2020(report)
-  report.split("\n")
+  report_array = report.split("\n")
+  results_array = []
+  report_array_numbers = report_array.map { |x| x.to_i }
+  report_array_numbers.each do |x|
+    if report_array_numbers.include?((2020 - x))
+      report_array_numbers.delete(x)
+      results_array.push(x)
+      results_array.push((2020 - x))
+    end
+  end
+  results_array
 end
--- spec/day1_spec.rb
+++ spec/day1_spec.rb
@@ -3,9 +3,8 @@ require "../lib/day1.rb"
 
 describe "sum2020" do
   
-  it "loads the report and splits it into an array"do
-    expect(sum2020(TEST)).to eq(["1721", "979", "366", "299", "675", "1456"])
-    p sum2020(TEST)
+  it "solves the test array" do
+    expect(sum2020(TEST)).to eq([1721, 299])
   end
 
 end
