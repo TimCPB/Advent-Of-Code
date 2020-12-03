c3dd0ff63f6f1a30e7acd8fbed332b929183cae6
Tim <timcpb@hotmail.com>
2020-12-03 23:54:58 +0000

    multiples are identified but the process continues afterwards

--- lib/day1.rb
+++ lib/day1.rb
@@ -11,8 +11,39 @@ def sum2020(report)
       results_array.push((2020 - x))
     end
   end
-  p results_array[0] * results_array[1]
+  results_array[0] * results_array[1]
 end
 
 def three_multiples(report)
+  report_array = report.split("\n")
+  results_array = []
+  report_array_numbers = report_array.map { |x| x.to_i }
+  report_array_numbers.each do |x|
+    # p x
+    # p report_array_numbers
+    target = 2020 - x
+    temp_results_array = []
+    temp_results_array.push(x)
+    # p temp_results_array
+    temp_report_array = report_array_numbers.reject{ |e| e == x }
+    # ary.reject!{|e| e==42 }
+    # p temp_report_array
+    temp_report_array.each do |z|
+      if temp_report_array.include?(target - z)
+        p temp_report_array
+        temp_report_array.delete(z)
+        temp_report_array.delete((target - z))
+        temp_results_array.push(z)
+        temp_results_array.push((target - z))
+        p temp_report_array
+        # p temp_results_array
+      end
+    end
+    results_array.push(temp_results_array[0])
+    results_array.push(temp_results_array[1])
+    results_array.push(temp_results_array[2])
+    # p results_array
+  end
+  # p results_array
+  results_array[0] * results_array[1] * results_array[2]
 end
