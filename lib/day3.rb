c6d553062850253fa579c42a096e4a9908cbe378
Tim <timcpb@hotmail.com>
2020-12-04 16:55:50 +0000

    counts trees in three rows of data

--- lib/day3.rb
+++ lib/day3.rb
@@ -1,7 +1,19 @@
 def count_trees(input)
 
-  
+  tree_count = 0
+  right = 3
+  down = 1
+  lateral_coord = 0
 
-  p input
+  input.each do |row|
+    
+    if row[lateral_coord] == "#"
+      tree_count += 1
+    end
+    lateral_coord += right
+
+  end
+
+  tree_count
 
 end
--- spec/day3_spec.rb
+++ spec/day3_spec.rb
@@ -1,11 +1,15 @@
 require '../lib/day3.rb'
 
 describe "count_trees" do
-  
-  input = File.readlines('./constants/day3_test.txt', chomp: true)
 
-  it "prints the initial test input" do
-    expect(count_trees(input)).to eq(input)
+  # it "prints the initial test input" do
+  #   input = File.readlines('./constants/day3_test.txt', chomp: true)
+  #   expect(count_trees(input)).to eq(input)
+  # end
+
+  it "counts trees in three rows of data" do
+    input = File.readlines('./constants/day3_basic.txt', chomp: true)
+    expect(count_trees(input)).to eq(1)
   end
-  
+
 end
