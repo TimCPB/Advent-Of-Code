491fa269f3c8a4e7d7767127ce460b39e8533b89
Tim <timcpb@hotmail.com>
2020-12-05 17:46:40 +0000

    turns each passport's data into a hash

--- lib/day4.rb
+++ lib/day4.rb
@@ -11,9 +11,21 @@ class Customs
     @formatted_data = ['']
   end
 
-  def separate_passports(data_array)
+  # def separate_passports(array)
+  #   i = 0
+  #   array.each do |line|
+  #     if line.empty?
+  #       i += 1
+  #       @formatted_data[i] = ''
+  #     else
+  #       @formatted_data[i] << ("#{line} ")
+  #     end
+  #   end
+  # end
+
+  def format_passports(array)
     i = 0
-    data_array.each do |line|
+    array.each do |line|
       if line.empty?
         i += 1
         @formatted_data[i] = ''
@@ -21,6 +33,20 @@ class Customs
         @formatted_data[i] << ("#{line} ")
       end
     end
+    @formatted_data.map! { |entry| entry.split(" ") }
+    @formatted_data.map! { |entry| categorise(entry) }
   end
+
+  def categorise(array)
+    hash = {}
+    array.each do |field|
+      sub_array = field.split(":")
+      hash[sub_array[0].to_sym] = sub_array[1]
+    end
+    hash
+  end
+
+  # def count_valid_passports
+
 
 end
--- spec/day4_spec.rb
+++ spec/day4_spec.rb
@@ -5,9 +5,18 @@ describe "Customs" do
   it "separates the passports and categories within the passports" do
     a = Customs.new
     input = a.test_data
-    a.separate_passports(input)
+    a.format_passports(input)
     expect(a.formatted_data.length).to eq(4)
-    expect(a.formatted_data[0].split(" ").length).to eq(8)
+    p a.formatted_data
+  end
+
+  it "splits each passport into categories" do
+    a = Customs.new
+    input = a.test_data
+    a.format_passports(input)
+    expect(a.formatted_data.length).to eq(4)
+    expect(a.formatted_data[0].length).to eq(8)
+    p a.formatted_data
   end
 
 end
