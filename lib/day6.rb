c38ae3d2ec7fcb8be6ff8040b40e519eaf3c415a
Tim <timcpb@hotmail.com>
2020-12-06 17:47:25 +0000

    counts the total number of unanimous answers

--- lib/day6.rb
+++ lib/day6.rb
@@ -1,11 +1,12 @@
 class Customs
 
-  attr_reader :test_data, :input_data, :formatted_answers
+  attr_reader :test_data, :input_data, :formatted_answers, :unanimous_answer_totals
 
   def initialize
     @test_data = File.readlines('./constants/day6_test.txt', chomp: true)
     @input_data = File.readlines('./constants/day6_input.txt', chomp: true)
     @formatted_answers = ['']
+    @unanimous_answer_totals = []
   end
 
   def format_answers(array)
@@ -18,8 +19,10 @@ class Customs
         @formatted_answers[i] << ("#{line} ")
       end
     end
-    @formatted_answers.map! { |entry| entry.gsub!(/\s+/, '') }
-    @formatted_answers.map! { |entry| entry.split("").uniq }
+    @formatted_answers.map! { |entry| entry.delete_suffix!(" ") }
+    @formatted_answers.map! { |entry| entry.split(" ") }
+    # @formatted_answers.map! { |entry| entry.gsub!(/\s+/, '') }
+    # @formatted_answers.map! { |entry| entry.split("").uniq }
     # @formatted_answers.map! { |entry| categorise(entry) }
   end
 
@@ -27,6 +30,35 @@ class Customs
     count = 0
     @formatted_answers.each { |arr| count += arr.length }
     count
+  end
+
+  def count_unanimous_answers(arr)
+    people = arr.length
+    count = 0
+    arr.map! { |x| x.split("")}
+    hash = {}
+    ('a'..'z').each do |letter|
+      hash[letter.to_sym] = 0
+    end
+    arr.each do |array|
+      array.each { |letter| hash[letter.to_sym] += 1}
+    end
+    hash.each do |letter, value|
+      if value == people
+        count += 1
+      end
+    end
+    count
+  end
+
+  def populate_unanimous_answers_totals
+    @formatted_answers.each do |answers|
+      @unanimous_answer_totals.push(count_unanimous_answers(answers))
+    end
+  end
+
+  def count_unanimous_answer_totals
+    @unanimous_answer_totals.sum
   end
 
 end
--- spec/day6_spec.rb
+++ spec/day6_spec.rb
@@ -7,22 +7,31 @@ describe "customs" do
     test = a.test_data
     a.format_answers(test)
     expect(a.formatted_answers.length).to eq(5)
+    p a.formatted_answers
     # expect(a.formatted_answers[1].length).to eq(3)
   end
 
-  it "creates an array for each group and removes duplicate answers" do
-    a = Customs.new
-    test = a.test_data
-    a.format_answers(test)
-    p a.formatted_answers
-    expect(a.formatted_answers[2].length).to eq(3)
-  end
+  # it "creates an array for each group and removes duplicate answers" do
+  #   a = Customs.new
+  #   test = a.test_data
+  #   a.format_answers(test)
+  #   expect(a.formatted_answers[2].length).to eq(3)
+  # end
 
-  it "sums the unique answers from each group" do
+  # it "sums the unique answers from each group" do
+  #   a = Customs.new
+  #   test = a.test_data
+  #   a.format_answers(test)
+  #   expect(a.count_answers).to eq(11)
+  # end
+
+  it "counts the total number of unanimous answers" do
     a = Customs.new
     test = a.test_data
     a.format_answers(test)
-    expect(a.count_answers).to eq(11)
+    a.populate_unanimous_answers_totals
+    p a.unanimous_answer_totals
+    expect(a.count_unanimous_answer_totals).to eq(6)
   end
 
 end
